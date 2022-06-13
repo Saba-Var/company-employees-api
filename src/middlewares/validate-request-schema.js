4e374ea45a9e607647bb0f9492a01940e790bd02
Saba-Var <vartasashvili94@gmail.com>
2022-06-13 13:35:11 +0400

    chore: return 422 if requiest body param is invalid

--- src/middlewares/validate-request-schema.js
+++ src/middlewares/validate-request-schema.js
@@ -3,7 +3,7 @@ import { validationResult } from 'express-validator'
 const validateRequestSchema = (req, res, next) => {
   const errors = validationResult(req)
   if (!errors.isEmpty()) {
-    return res.status(401).json({ errors: errors.array() })
+    return res.status(422).json({ errors: errors.array() })
   }
   return next()
 }
