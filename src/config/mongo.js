033db1fed14b508887a08822c53a6a764b83820b
Saba-Var <vartasashvili94@gmail.com>
2022-06-16 15:42:07 +0400

    chore: make connectionURL string dinamicaly

--- src/config/mongo.js
+++ src/config/mongo.js
@@ -2,12 +2,7 @@ import mongoose from 'mongoose'
 
 const connect = async () => {
   try {
-    let connectionURL
-
-    if (process.env.MONGO_HOST === 'localhost')
-      connectionURL = `mongodb://${process.env.MONGO_HOST}:${process.env.MONGO_PORT}/${process.env.MONGO_DATABASE}`
-    else
-      connectionURL = `mongodb+srv://${process.env.MONGO_HOST}:${process.env.MONGO_PORT}@cluster0.m3pxc13.mongodb.net/${process.env.MONGO_DATABASE}`
+    const connectionURL = `${process.env.MONGO_PROTOCOL}://${process.env.MONGO_HOST}:${process.env.MONGO_PORT}${process.env.MONGO_CLUSTER}/${process.env.MONGO_DATABASE}`
 
     return mongoose.connect(connectionURL, {
       useNewUrlParser: true,
